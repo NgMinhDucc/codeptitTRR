01aab69e4d9f62d80a68a28ef02ae4072b00ac2a
NgMinhDucc <ducnm@gdscptit.dev>
2026-05-13 09:15:58 +0700

    update gitignore

---
+++ TRR_1008_1_8_Do_Thi.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    // freopen("DT.INP", "r", stdin);
+    // freopen("DT.OUT", "w", stdout);
+
+    cout << "Hello";
+    return 0;
+}
