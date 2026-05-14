7c19efaca6c51506ef621aeba2b0f917ecf2bc20
NgMinhDucc <ducnm@gdscptit.dev>
2026-05-14 23:20:17 +0700

    May 14 2026

--- TRR_2015_2_15_Lien_Thong.cpp
+++ TRR_2015_2_15_Lien_Thong.cpp
@@ -15,12 +15,42 @@ void dfs(int u){
         }
     }
 }
+void dfs_undirected(int u){
+    visited[u] = true;
+    for (int i = 1; i <= n; i++){
+        if (!visited[i] && (a[u][i] || a[i][u])){
+            dfs_undirected(i);
+        }
+    }
+}
+
+string check(){
+    int cnt = 0;
+    for (int i = 1; i <= n; i++){
+        if (!visited[i]){
+            cnt += 1;
+            dfs_undirected(i);
+        }
+    }
+    if (cnt > 1){
+        return "0"; // neither strongly nor weakly connected
+    }
+    for (int i = 1; i <= n; i++){
+        visited.assign(n + 1, false);
+        path.clear();
+        dfs(i);
+        if ((int)path.size() != n){
+            return "2"; // weakly connected
+        }
+    }
+    return "1"; // strongly connected
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-    // freopen("TK.INP", "r", stdin);
-    // freopen("TK.OUT", "w", stdout);
+    freopen("TK.INP", "r", stdin);
+    freopen("TK.OUT", "w", stdout);
 
     cin >> n;
     a.assign(n + 1, vector<int>(n + 1));
@@ -30,5 +60,7 @@ int main(){
             cin >> a[i][j];
         }
     }
+
+    cout << check();
     return 0;
 }
