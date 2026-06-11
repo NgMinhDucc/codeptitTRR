fb71414f3e02380c77f4493a2685ea40c079163b
NgMinhDucc <ducnm@gdscptit.dev>
2026-06-11 22:21:10 +0700

    June 11 2026

--- TRR_1002_1_2_Do_Thi.cpp
+++ TRR_1002_1_2_Do_Thi.cpp
@@ -2,45 +2,42 @@
 using namespace std;
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-
+    ios::sync_with_stdio(false);
+    cin.tie(0);
     freopen("DT.INP", "r", stdin);
     freopen("DT.OUT", "w", stdout);
 
     int t, n;
     cin >> t >> n;
+    vector<int> deg(n + 1);
     vector<vector<int>> a(n + 1);
-    vector<vector<int>> adj(n + 1);
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= n; j++){
             int x;
             cin >> x;
-            if (x == 1 && i < j){
-                a[i].push_back(j);
-                a[j].push_back(i);
-                adj[i].push_back(j);
-                adj[j].push_back(i);
+            if (x == 1){
+                if (i < j){
+                    a[i].push_back(j);
+                    a[j].push_back(i);
+                }
+                deg[i] += 1;
             }
         }
     }
-
     if (t == 1){
         for (int i = 1; i <= n; i++){
-            cout << a[i].size() << " ";
+            cout << deg[i] << " ";
         }
     }
-    else{
+    if (t == 2){
         cout << n << endl;
         for (int i = 1; i <= n; i++){
             cout << a[i].size() << " ";
-            for (int j = 0; j < (int)adj[i].size(); j++){
-                cout << adj[i][j] << " ";
+            for (int j = 0; j < (int)a[i].size(); j++){
+                cout << a[i][j] << " ";
             }
             cout << endl;
         }
     }
-
     return 0;
 }
