ae86dced2cf5c59a7225c48cca520aebada59301
NgMinhDucc <ducnm@gdscptit.dev>
2026-06-13 10:49:36 +0700

    June 13 2026

--- TRR_1010_1_10_Do_Thi.cpp
+++ TRR_1010_1_10_Do_Thi.cpp
@@ -1,47 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+struct edge{
+    int u, v, w;
+};
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
-
-    // freopen("DT.INP", "r", stdin);
-    // freopen("DT.OUT", "w", stdout);
+    freopen("DT.INP", "r", stdin);
+    freopen("DT.OUT", "w", stdout);
 
     int t, n;
     cin >> t >> n;
-    vector<vector<int>> a(n + 1, vector<int>(n + 1, 0));
-    int m = 0;
+    vector<int> bac(n + 1, 0);
+    vector<edge> canh;
     for (int i = 1; i <= n; i++){
         for (int j = 1; j <= n; j++){
             int x;
             cin >> x;
-            if (x != 0 && x != 10000 && i < j){
-                m += 1;
-                a[i][j] = a[j][i] = x;
+            if (x != 10000 && x != 0){
+                bac[i] += 1;
+                if (i < j){
+                    canh.push_back({i, j, x});
+                }
             }
         }
     }
-
     if (t == 1){
         for (int i = 1; i <= n; i++){
-            int deg = 0;
-            for (int j = 1; j <= n; j++){
-                if (a[i][j]){
-                    deg += 1;
-                }
-            }
-            cout << deg << " ";
+            cout << bac[i] << " ";
         }
     }
     if (t == 2){
-        cout << n << " " << m << "\n";
-        for (int i = 1; i <= n; i++){
-            for (int j = 1; j <= n; j++){
-                if (a[i][j] && i < j){
-                    cout << i << " " << j << " " << a[i][j] << "\n";
-                }
-            }
+        int m = canh.size();
+        cout << n << " " << m << endl;
+        for (int i = 0; i < m; i++){
+            cout << canh[i].u << " " << canh[i].v << " " << canh[i].w << endl;
         }
     }
     return 0;
