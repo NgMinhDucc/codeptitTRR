2c184894d645028e401c5a8c3229db02882b3e4e
NgMinhDucc <ducnm@gdscptit.dev>
2026-06-14 21:49:16 +0700

    June 14 2026

--- TRR_3001_3_1_Chu_Trinh_Euler.cpp
+++ TRR_3001_3_1_Chu_Trinh_Euler.cpp
@@ -3,102 +3,96 @@ using namespace std;
 
 int t, n, u;
 vector<vector<int>> a;
+vector<int> deg;
 vector<bool> visited;
-vector<int> degree;
 
 void dfs(int u){
-    visited[u] = true;
-    for (int i = 1; i <= n; i++){
-        if (a[u][i] == 1 && !visited[i]){
-            dfs(i);
-        }
-    }
+	visited[u] = true;
+	for (int i = 1; i <= n; i++){
+		if (!visited[i] && a[u][i]){
+			dfs(i);
+		}
+	}
 }
 
-bool lienthong(){
-    int s = -1;
-    for (int i = 1; i <= n; i++){
-        if (degree[i] > 0){
-            s = i;
-            break;
-        }
-    }
-    if (s == -1){
-        return true; // khong co canh nao --> euler
-    }
-
-    dfs(s);
-    for (int i = 1; i <= n; i++){
-        if (degree[i] > 0 && !visited[i]){
-            return false; // khong lien thong
-        }
-    }
-    return true;
+bool lien_thong(){
+	int cnt = 0;
+	for (int i = 1; i <= n; i++){
+		if (!visited[i] && deg[i] > 0){
+			cnt += 1;
+			dfs(i);
+		}
+	}
+	if (cnt > 1){
+		return false;
+	}
+	return true;
 }
 
 void euler(int u){
-    vector<int> ec;
-    stack<int> st;
-    st.push(u);
-    while (!st.empty()){
-        int x = st.top();
-        bool flag = false;
-        for (int i = 1; i <= n; i++){
-            if (a[x][i]){
-                st.push(i);
-                a[x][i] = a[i][x] = 0;
-                flag = true;
-                break;
-            }
-        }
-        if (!flag){
-            st.pop();
-            ec.push_back(x);
-        }
-    }
-    for (int i = ec.size() - 1; i >= 0; i--){
-        cout << ec[i] << " ";
-    }
+	stack<int> st;
+	st.push(u);
+	vector<int> ec;
+	while (!st.empty()){
+		int s = st.top();
+		bool flag = false;
+		for (int i = 1; i <= n; i++){
+			if (a[s][i]){
+				st.push(i);
+				flag = true;
+				a[s][i] = a[i][s] = 0;
+				break;
+			}
+		}
+		if (!flag){
+			st.pop();
+			ec.push_back(s);
+		}
+	}
+	for (int i = ec.size() - 1; i >= 0; i--){
+		cout << ec[i] << " ";
+	}
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    freopen("CT.INP", "r", stdin);
-    freopen("CT.OUT", "w", stdout);
-    cin >> t >> n;
-    if (t == 2){
-        cin >> u;
-    }
-    a.assign(n + 1, vector<int>(n + 1));
-    visited.assign(n + 1, false);
-    degree.assign(n + 1, 0);
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            cin >> a[i][j];
-            if (a[i][j] == 1){
-                degree[i] += 1;
-            }
-        }
-    }
-    if (t == 1){
-        if (!lienthong()){
-            cout << 0; // khong lien thong --> khong euler cung khong nua euler
-        }
-        else{
-            int cnt = 0;
-            for (int i = 1; i <= n; i++){
-                if (degree[i] % 2 == 1){
-                    cnt += 1;
-                }
-            }
-            if (cnt == 0) cout << 1; // khong co dinh bac le --> euler
-            else if (cnt == 2) cout << 2; // co dung 2 dinh bac le --> nua euler
-            else cout << 0; // con lai
-        }
-    }
-    if (t == 2){
-        euler(u);
-    }
-    return 0;
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	freopen("CT.INP", "r", stdin);
+	freopen("CT.OUT", "w", stdout);
+
+	cin >> t >> n;
+	if (t == 2){
+		cin >> u;
+	}
+	a.assign(n + 1, vector<int>(n + 1));
+	deg.assign(n + 1, 0);
+	visited.assign(n + 1, false);
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= n; j++){
+			cin >> a[i][j];
+			if (a[i][j]){
+				deg[i] += 1;
+			}
+		}
+	}
+	if (t == 1){
+		if (!lien_thong()){
+			cout << 0; // khong lien thong
+		}
+		else{
+			int cnt = 0;
+			for (int i = 1; i <= n; i++){
+				if (deg[i] % 2 == 1){
+					cnt += 1;
+				}
+			}
+			if (cnt == 0) cout << 1; // euler
+			else if (cnt == 2) cout << 2; // nua euler
+			else cout << 0;
+		}
+	}
+	else{
+		euler(u);
+	}
+	return 0;
 }
--- TRR_3004_3_4_Chu_Trinh_Euler.cpp
+++ TRR_3004_3_4_Chu_Trinh_Euler.cpp
@@ -4,102 +4,98 @@ using namespace std;
 int t, n, u;
 vector<vector<int>> a;
 vector<bool> visited;
-vector<int> ra, vao;
+vector<int> vao, ra;
 
 void dfs(int u){
-    visited[u] = true;
-    for (int i = 1; i <= n; i++){
-        if ((a[u][i] == 1 || a[i][u] == 1) && !visited[i]){
-            dfs(i);
-        }
-    }
+	visited[u] = true;
+	for (int i = 1; i <= n; i++){
+		if (!visited[i] && a[u][i]){
+			dfs(i);
+		}
+	}
 }
 
-bool lienthong(){
-    int s = -1;
-    for (int i = 1; i <= n; i++){
-        if (ra[i] > 0 || vao[i] > 0){
-            s = i;
-            break;
-        }
-    }
-    if (s == -1) return true;
-
-    dfs(s);
-    for (int i = 1; i <= n; i++){
-        if ((ra[i] > 0 || vao[i] > 0) && !visited[i]) return false;
-    }
-    return true;
+bool lien_thong(){
+	int cnt = 0;
+	for (int i = 1; i <= n; i++){
+		if (!visited[i] && (vao[i] > 0 || ra[i] > 0)){
+			cnt += 1;
+			dfs(i);
+		}
+	}
+	if (cnt > 1){
+		return false;
+	}
+	return true;
 }
 
 void euler(int u){
-    vector<int> ec;
-    stack<int> st;
-    st.push(u);
-    while (!st.empty()){
-        int x = st.top();
-        bool flag = false;
-        for (int i = 1; i <= n; i++){
-            if (a[x][i] == 1){
-                st.push(i);
-                a[x][i] = 0;
-                flag = true;
-                break;
-            }
-        }
-        if (!flag){
-            st.pop();
-            ec.push_back(x);
-        }
-    }
-    for (int i = ec.size() - 1; i >= 0; i--){
-        cout << ec[i] << " ";
-    }
+	stack<int> st;
+	st.push(u);
+	vector<int> ec;
+	while (!st.empty()){
+		int s = st.top();
+		bool flag = false;
+		for (int i = 1; i <= n; i++){
+			if (a[s][i]){
+				st.push(i);
+				a[s][i] = 0;
+				flag = true;
+				break;
+			}
+		}
+		if (!flag){
+			st.pop();
+			ec.push_back(s);
+		}
+	}
+	for (int i = ec.size() - 1; i >= 0; i--){
+		cout << ec[i] << " ";
+	}
 }
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    freopen("CT.INP", "r", stdin);
-    freopen("CT.OUT", "w", stdout);
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	freopen("CT.INP", "r", stdin);
+	freopen("CT.OUT", "w", stdout);
 
-    cin >> t >> n;
-    if (t == 2){
-        cin >> u;
-    }
-    a.assign(n + 1, vector<int>(n + 1));
-    visited.assign(n + 1, false);
-    ra.assign(n + 1, 0);
-    vao.assign(n + 1, 0);
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
-            cin >> a[i][j];
-            if (a[i][j] == 1){
-                ra[i] += 1;
-                vao[j] += 1;
-            }
-        }
-    }
-    if (t == 1){
-        if (!lienthong()){
-            cout << 0;
-        }
-        else{
-            int rahon = 0, vaohon = 0, khonghople = 0;
-            for (int i = 1; i <= n; i++){
-                if (ra[i] == vao[i]) continue;
-                else if (ra[i] - vao[i] == 1) rahon += 1;
-                else if (vao[i] - ra[i] == 1) vaohon += 1;
-                else khonghople += 1;
-            }
-
-            if (rahon == 0 && vaohon == 0 && khonghople == 0) cout << 1; // euler
-            else if (rahon == 1 && vaohon == 1 && khonghople == 0) cout << 2; // nua euler
-            else cout << 0; // khong euler cung khong nua euler
-        }
-    }
-    if (t == 2){
-        euler(u);
-    }
-    return 0;
+	cin >> t >> n;
+	if (t == 2){
+		cin >> u;
+	}
+	a.assign(n + 1, vector<int>(n + 1));
+	visited.assign(n + 1, false);
+	vao.assign(n + 1, 0);
+	ra.assign(n + 1, 0);
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= n; j++){
+			cin >> a[i][j];
+			if (a[i][j]){
+				vao[j] += 1;
+				ra[i] += 1;
+			}
+		}
+	}
+	if (t == 1){
+		if (!lien_thong()){
+			cout << 0;	
+		}
+		else{
+			int rahon = 0, vaohon = 0, invalid = 0;
+			for (int i = 1; i <= n; i++){
+				if (ra[i] == vao[i]) continue;
+				else if (ra[i] - vao[i] == 1) rahon += 1;
+				else if (vao[i] - ra[i] == 1) vaohon += 1;
+				else invalid += 1;
+			}
+			if (rahon == 0 && vaohon == 0 && invalid == 0) cout << 1;
+			else if (rahon == 1 && vaohon == 1 && invalid == 0) cout << 2;
+			else cout << 0;
+		}
+	}
+	if (t == 2){
+		euler(u);
+	}
+	return 0;
 }
